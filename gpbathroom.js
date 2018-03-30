01edde9baa9307b8aa5c5b911ffadc1a60d1dd09
Justin Halladay <justinhalladay@justins-mbp.uconnect.utah.edu>
2018-03-29 21:39:50 -0600

    location

--- gpbathroom.js
+++ gpbathroom.js
@@ -1,14 +1,51 @@
 console.log('working')
 var dropdown = ['Gender Neutral', 'Family', 'Single Stall'];
 
+// function initMap() {
+//   console.log('loading map')
+//   var uofu = { lat: 40.766829464704202, lng: -111.84779251030393, }
+//   var map = new google.maps.Map(document.getElementById('map-body'), {
+//     center: uofu,
+//     zoom: 15,
+//   });
+//   console.log('MAP DATA', map)
+// }
+
+var map, infoWindow;
 function initMap() {
-  console.log('loading map')
-  var uofu = { lat: 40.766829464704202, lng: -111.84779251030393, }
-  var map = new google.maps.Map(document.getElementById('map-body'), {
-    center: uofu,
-    zoom: 15,
+  map = new google.maps.Map(document.getElementById('map-body'), {
+    center: { lat: 40.766829464704202, lng: -111.84779251030393, },
+    zoom: 15
   });
-  console.log('MAP DATA', map)
+  infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function () {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
 }
 
 $(function () {
@@ -70,24 +107,24 @@ function rate_images(rating) {
 }
 // 5 star rating
 
-// //This is the code to find your current location. 
-var x = document.getElementById("demo");
+// // //This is the code to find your current location. 
+// var x = document.getElementById("demo");
 
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition());
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
+// function getLocation() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(showPosition());
+//   } else {
+//     x.innerHTML = "Geolocation is not supported by this browser.";
+//   }
+// }
 
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
-console.log();
-};
-console.log("____________________");
-getLocation();
+// function showPosition(position) {
+//   x.innerHTML = "Latitude: " + position.coords.latitude +
+//     "<br>Longitude: " + position.coords.longitude;
+// console.log();
+// };
+// console.log("____________________");
+// getLocation();
 
 
 
